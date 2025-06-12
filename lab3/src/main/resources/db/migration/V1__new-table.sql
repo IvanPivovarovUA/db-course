4226b3b3d261d566a082a5d5872e8b2828b4f1ca
Ivan <ivanpivovarovua@gmail.com>
2025-06-13 02:02:38 +0300

    Lab3 start. Connect to db and test flyway

---
+++ lab3/src/main/resources/db/migration/V1__new-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE weather (
+    USER_ID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    country VARCHAR,
+    last_updated VARCHAR,
+    air_quality_Carbon_Monoxide FLOAT,
+    air_quality_Ozone FLOAT,
+    air_quality_Nitrogen_dioxide FLOAT,
+    air_quality_Sulphur_dioxide FLOAT,
+    air_quality_PM2_5 FLOAT,
+    air_quality_PM10 FLOAT,
+    air_quality_us_epa_index FLOAT,
+    air_quality_gb_defra_index FLOAT
+);
